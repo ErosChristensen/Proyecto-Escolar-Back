c26a737095983199330c77f6cc4f70d667dffa61
Ludmilameir <ludmilameira201@gmail.com>
2025-10-15 10:08:44 -0300

    aa

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import preInscripcionForm from "./routes/preInscripcionForm.js";
 import formularioRoutes from "./routes/formulario.js";
 import AdminRegistro from "./routes/admin.registro.js";
 import preInscripcionFunction from "./routes/preInscripcionFunction.js";
-import meRouter from "./routes/me.js";
+
 dotenv.config();  
 
 const app = express();
@@ -49,10 +49,8 @@ app.use("/api/admin/noticias", AdminNoticias);
 app.use("/api/preinscripcion", preInscripcionForm);
 app.use("/api/admin/registro", AdminRegistro);
 app.use("/api/admin/registro", preInscripcionFunction);
-app.use("/api/me", meRouter);
 //Agregar los RequireAdmin a la de AdminRegistro, AdminNoticias y preInscripcionFunction
-app.use("/api/admin", preInscripcionRoutes);
-app.use("/api/me", meRouter);
+
 // Root
 app.get("/", (_req, res) => {
 res.send("Servidor backend funcionando");
